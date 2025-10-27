bb091c5cb1e3f8884d1284e43b09570aee334d90
Jonathan Schoonhoven <jschoonhoven@chanzuckerberg.com>
2025-10-27 14:34:17 -0700

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="container">
+      <h1 className="title">Schoon<br/>hoven<br/>dot.<br/>com</h1>
+      <div className="message">
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <strong>Hello, world.</strong> You found my personal email server. If you want to get in touch, find me on <a href="https://github.com/jtschoonhoven" target="_blank">Github</a> or <a href="https://www.linkedin.com/in/jtschoonhoven/" target="_blank">LinkedIn</a>.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
